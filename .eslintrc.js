6f9185cf4a9a8b365f50914d8bbed0341bd0c7c6
akram <akram.harazem@gmail.com>
2024-08-11 21:28:17 +0300

    setup eslint,babel.config, tsConfig and reactNative config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,17 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'no-unused-vars': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    'no-shadow': 'off',
+    'react/react-in-tsx-scope': 'off',
+    'react/no-unstable-nested-components': 'off',
+    eqeqeq: 'off',
+    quotes: [
+      'error',
+      'single',
+      {avoidEscape: true, allowTemplateLiterals: true},
+    ],
+  },
 };
