9e07ae6820514685816bbd7d6505deef0d14e59e
akram <akram.harazem@gmail.com>
2024-08-16 19:18:01 +0300

    add confirm alert for logout

--- app/hooks/useHandleLogout.ts
+++ app/hooks/useHandleLogout.ts
@@ -2,19 +2,36 @@ import screenNames, {useMainNavigation} from '@common/screensConfig';
 import {api} from '@services/api';
 import {resetToken} from '@slices/authSlices';
 import {useCallback} from 'react';
+import {useTranslation} from 'react-i18next';
+import {Alert} from 'react-native';
 import {useDispatch} from 'react-redux';
 
 const useHandleLogout = () => {
   const dispatch = useDispatch();
+  const {t} = useTranslation();
   const navigation = useMainNavigation();
-  const handleLogOut = useCallback(() => {
+  const logout = useCallback(() => {
     dispatch(resetToken());
-    dispatch(api.util.resetApiState());
     navigation.reset({
       index: 0,
       routes: [{name: screenNames.RootStack.AuthStack}],
     });
+    dispatch(api.util.resetApiState());
   }, []);
+  const handleLogOut = useCallback(() => {
+    Alert.alert(t('sign_out'), t('sure_logout'), [
+      {
+        text: t('cancel'),
+        style: 'cancel',
+      },
+      {
+        text: t('confirm'),
+        onPress: logout,
+        style: 'destructive',
+      },
+    ]);
+  }, []);
+
   return {handleLogOut};
 };
 
